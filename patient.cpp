25ca05779c57341f7e4ede6b080254be873f6e18
jawad <221085796+jawadyaics@users.noreply.github.com>
2026-05-01 09:51:49 +0500

    Implement Patient class inherit from Person class create patient.cpp and patient.h and implement core attributes of patient

---
+++ patient.cpp
@@ -0,0 +1,70 @@
+#include "patient.h"
+
+    Patient::Patient(){
+        Age = 0;
+        Gender[1] = '\0';
+        Contact[1] = '\0';
+        Balance = 0;
+    }
+
+
+    Patient::Patient(int id ,const char* name ,const char* password ,int age ,const char* gender ,const char* contact, double balance)
+    : Person(id , name , password), Age(age), Balance(balance)
+    {
+        StringCopy(Gender, gender);
+        StringCopy(Contact, contact);
+    }
+
+    // getter
+
+    int Patient::getAge() const {
+        return Age;
+    }
+
+    const char* Patient::getGender() const{
+        return Gender;
+    }
+
+    const char* Patient::getContact() const{
+        return Contact;
+    }
+
+    double Patient::getBalance() const {
+        return Balance;
+    }
+
+
+    // Setters 
+
+    void Patient::setAge(int age) {
+        Age = age;
+    }
+
+    void Patient::setGender(const char* gender){
+        StringCopy(Gender , gender);
+    }
+
+    void Patient::setContact(const char* contact){
+        StringCopy(Contact, contact);
+    }
+
+    void Patient::setBalance(double balance){
+        Balance = balance;
+    }
+
+
+    void Patient::addFunds(double amount){
+        if (amount > 0) Balance+=amount;
+    }
+
+    void Patient::deductFunds(double amount){
+        Balance -= amount;
+    }
+
+    void Patient::showMenu(){
+
+    }
+
+    void Patient::printDetails() {
+
+    }
---
+++ patient.h
@@ -0,0 +1,48 @@
+#pragma once
+#include "person.h"
+
+
+class Patient : public Person {
+    private:
+    int Age;
+    char Gender[20];
+    char Contact[20];
+    double Balance;
+
+    public:
+    Patient(){};
+    Patient(int id ,const char* name ,const char* password ,int age ,const char* gender ,const char* contact, double balance){};
+
+    ~Patient();
+
+    // getter
+
+    int getAge() const {};
+
+    const char* getGender() const{};
+
+    const char* getContact() const{};
+
+    double getBalance() const {};
+
+
+    // Setters 
+
+    void setAge(int age) {};
+
+    void setGender(const char* gender){};
+
+    void setContact(const char* contact){};
+
+    void setBalance(double balance){};
+
+
+    void addFunds(double amount){};
+
+    void deductFunds(double abount){};
+
+    // Virtual Functions
+
+    void showMenu(){};
+    void printDetails(){};
+};
